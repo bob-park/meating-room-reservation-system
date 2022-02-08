805731178ab5b912bfc8625961dfa7e311ad300b
Bob Park <hwpark11@kakao.com>
2022-02-08 13:32:16 +0900

    M2RS-3 feat: config service 모듈 추가

---
+++ config-service/src/main/java/com/m2rs/configservice/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.m2rs.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
