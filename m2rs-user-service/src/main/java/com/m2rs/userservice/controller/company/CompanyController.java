d30e027f5b564494a066ca97b787890bad367f64
Bob Park <hwpark11@kakao.com>
2022-02-11 15:40:49 +0900

    M2RS-16 feat: company 로고 변경 API 추가

--- m2rs-user-service/src/main/java/com/m2rs/userservice/controller/company/CompanyController.java
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/controller/company/CompanyController.java
@@ -2,14 +2,19 @@ package com.m2rs.userservice.controller.company;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.m2rs.core.commons.model.api.response.ApiResult;
+import com.m2rs.core.model.Id;
 import com.m2rs.userservice.model.api.company.CompanyDto;
 import com.m2rs.userservice.model.api.company.CreateCompany;
+import com.m2rs.userservice.model.entity.Company;
 import com.m2rs.userservice.service.company.CompanyService;
 
 import static com.m2rs.core.commons.model.api.response.ApiResult.OK;
@@ -24,6 +29,12 @@ public class CompanyController {
     @PostMapping(path = "")
     public ApiResult<CompanyDto> createCompany(@RequestBody CreateCompany createCompany) {
         return OK(companyService.createCompany(createCompany));
+    }
+
+    @PostMapping(path = "{companyId}/logo")
+    public ApiResult<CompanyDto> changeLogo(@PathVariable Long companyId,
+        @RequestPart MultipartFile logo) {
+        return OK(companyService.changeLogo(Id.of(Company.class, companyId), logo));
     }
 
 }
--- m2rs-user-service/src/main/java/com/m2rs/userservice/service/company/impl/CompanyServiceImpl.java
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/service/company/impl/CompanyServiceImpl.java
@@ -3,9 +3,24 @@ package com.m2rs.userservice.service.company.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.m2rs.core.commons.exception.NotFoundException;
+import com.m2rs.core.commons.exception.ServiceRuntimeException;
+import com.m2rs.core.model.Id;
+import com.m2rs.userservice.configure.user.properties.UserProperties;
 import com.m2rs.userservice.model.api.company.CompanyDto;
 import com.m2rs.userservice.model.api.company.CreateCompany;
 import com.m2rs.userservice.model.entity.Company;
@@ -20,6 +35,8 @@ import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 @Service
 @Transactional(readOnly = true)
 public class CompanyServiceImpl implements CompanyService {
+
+    private final UserProperties userProperties;
 
     private final CompanyRepository companyRepository;
 
@@ -38,9 +55,50 @@ public class CompanyServiceImpl implements CompanyService {
         return CompanyDto.builder()
             .id(company.getId())
             .name(company.getName())
+            .logoPath(company.getLogoPath())
             .createdDate(company.getCreatedDate())
             .lastModifiedDate(company.getLastModifiedDate())
             .build();
+    }
+
+    @Transactional
+    @Override
+    public CompanyDto changeLogo(Id<Company, Long> id, MultipartFile logoFile) {
+
+        Company company = companyRepository
+            .findById(id.value())
+            .orElseThrow(() -> new NotFoundException(id.getClass(), id.value()));
+
+        String logoDir = userProperties.getCompany().getLogoDir();
+
+        String fileName = UUID.randomUUID().toString();
+        String extension = FilenameUtils.getExtension(logoFile.getOriginalFilename());
+
+        String logoPath = company.getId() + ""
+            + File.separatorChar
+            + fileName
+            + "."
+            + extension;
+
+        log.debug("logo_path={}", logoPath);
+
+        try {
+            FileUtils.copyInputStreamToFile(logoFile.getInputStream(),
+                new File(logoDir + File.separatorChar + logoPath));
+        } catch (IOException e) {
+            throw new ServiceRuntimeException(e.getMessage());
+        }
+
+        company.addLogoPath(logoPath);
+
+        return CompanyDto.builder()
+            .id(company.getId())
+            .name(company.getName())
+            .logoPath(company.getLogoPath())
+            .createdDate(company.getCreatedDate())
+            .lastModifiedDate(company.getLastModifiedDate())
+            .build();
+
     }
 
 }
