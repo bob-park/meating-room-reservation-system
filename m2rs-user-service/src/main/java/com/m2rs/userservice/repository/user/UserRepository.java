df023d860d2a4867b5f53ca08015a8836f99bcb2
Bob Park <hwpark11@kakao.com>
2022-03-08 17:40:50 +0900

    M2RS-21 feat: user repository 에 email 로 사용자 조회 메서드 추가

--- m2rs-user-service/src/main/java/com/m2rs/userservice/repository/user/UserRepository.java
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/repository/user/UserRepository.java
@@ -1,8 +1,11 @@
 package com.m2rs.userservice.repository.user;
 
 import com.m2rs.userservice.model.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
 }
