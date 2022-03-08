a6e780b8a0469da1b62b1c25a30cd1fa95bccde8
Bob Park <hwpark11@kakao.com>
2022-03-08 17:41:56 +0900

    M2RS-21 feat: email 로 사용자 조회하는 request dto 추가

---
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/model/api/user/CheckUserRequest.java
@@ -0,0 +1,14 @@
+package com.m2rs.userservice.model.api.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CheckUserRequest {
+
+    private String email;
+
+}
