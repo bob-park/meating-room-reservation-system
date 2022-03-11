a459da8dce82e14d61f72e08e81a7fb7e0c13880
Bob Park <hwpark11@kakao.com>
2022-03-11 14:30:36 +0900

    M2RS-21 feat: role repository 추가

---
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/repository/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.m2rs.userservice.repository.role;
+
+import com.m2rs.userservice.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+
+}
