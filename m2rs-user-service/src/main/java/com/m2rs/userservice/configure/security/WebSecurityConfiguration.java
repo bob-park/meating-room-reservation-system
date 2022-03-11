1d9f60a44a7089676cd893a0ca37d818d41a906b
Bob Park <hwpark11@kakao.com>
2022-03-11 13:25:57 +0900

    M2RS-21 feat: 사용자 로그인을 DB 와 연동 및 authentication provider 연동

--- m2rs-user-service/src/main/java/com/m2rs/userservice/configure/security/WebSecurityConfiguration.java
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/configure/security/WebSecurityConfiguration.java
@@ -1,21 +1,39 @@
 package com.m2rs.userservice.configure.security;
 
 
+import com.m2rs.core.security.model.JwtClaimInfo;
+import com.m2rs.userservice.security.entrypoint.RestLoginAuthenticationEntryPoint;
+import com.m2rs.userservice.security.filter.RestLoginProcessingFilter;
+import com.m2rs.userservice.security.handler.RestAccessDeniedHandler;
+import com.m2rs.userservice.security.handler.RestAuthenticationFailureHandler;
+import com.m2rs.userservice.security.handler.RestAuthenticationSuccessHandler;
+import com.m2rs.userservice.security.provider.CustomAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtClaimInfo jwtClaimInfo;
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     public void configure(WebSecurity web) {
@@ -24,19 +42,65 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(getAuthenticationProvider());
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+            .antMatchers(RestLoginProcessingFilter.REST_LOGIN_URI).permitAll()
             .anyRequest()
-            .permitAll();
+            .authenticated();
+
+        http.exceptionHandling()
+            .authenticationEntryPoint(new RestLoginAuthenticationEntryPoint())
+            .accessDeniedHandler(getAccessDeniedHandler());
 
         http.formLogin().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .csrf().disable()
             .cors().disable();
     }
 
     @Bean
+    public RestLoginProcessingFilter getRestLoginProcessingFilter() throws Exception {
+        RestLoginProcessingFilter restLoginProcessingFilter = new RestLoginProcessingFilter();
+
+        restLoginProcessingFilter
+            .setAuthenticationManager(authenticationManagerBean());
+        restLoginProcessingFilter
+            .setAuthenticationSuccessHandler(getAuthenticationSuccessHandler());
+        restLoginProcessingFilter
+            .setAuthenticationFailureHandler(getAuthenticationFailureHandler());
+
+        return restLoginProcessingFilter;
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider getAuthenticationProvider() {
+        return new CustomAuthenticationProvider(userDetailsService, passwordEncoder());
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler getAuthenticationSuccessHandler() {
+        return new RestAuthenticationSuccessHandler(jwtClaimInfo);
+    }
+
+    @Bean
+    public AuthenticationFailureHandler getAuthenticationFailureHandler() {
+        return new RestAuthenticationFailureHandler();
+    }
+
+    @Bean
+    public AccessDeniedHandler getAccessDeniedHandler() {
+        return new RestAccessDeniedHandler();
     }
 }
