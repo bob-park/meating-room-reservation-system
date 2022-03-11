3425e0046450871d380337433c32cf571882deb3
Bob Park <hwpark11@kakao.com>
2022-03-11 17:50:05 +0900

    M2RS-21 refactor: 사용자 확인 controller 에서 authentication 에서 받아올 수 있도록 수정

--- m2rs-user-service/src/main/java/com/m2rs/userservice/controller/user/UserController.java
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/controller/user/UserController.java
@@ -4,12 +4,12 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 import com.m2rs.core.commons.model.api.response.ApiResult;
-import com.m2rs.userservice.model.api.user.CheckUserRequest;
 import com.m2rs.userservice.model.api.user.UserResponse;
+import com.m2rs.userservice.security.model.RestAuthentication;
 import com.m2rs.userservice.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +21,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping(path = "check")
-    public ApiResult<UserResponse> checkUser(@RequestBody CheckUserRequest checkUserRequest) {
+    public ApiResult<UserResponse> checkUser(
+        @AuthenticationPrincipal RestAuthentication authentication) {
 
-        checkArgument(isNotEmpty(checkUserRequest.getEmail()), "email must be provided.");
+        checkArgument(isNotEmpty(authentication.getEmail()), "email must be provided.");
 
-        return ApiResult.ok(userService.getUser(checkUserRequest.getEmail()));
+        return ApiResult.ok(userService.getUser(authentication.getEmail()));
 
     }
 
