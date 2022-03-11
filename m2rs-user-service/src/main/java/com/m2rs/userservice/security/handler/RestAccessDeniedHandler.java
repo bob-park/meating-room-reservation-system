ba81666a2a607885ecd84192f92759195c4011c0
Bob Park <hwpark11@kakao.com>
2022-03-11 12:26:24 +0900

    M2RS-21 feat: security Exception Handler 추가

---
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/security/handler/RestAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.m2rs.userservice.security.handler;
+
+import static com.m2rs.core.commons.model.api.response.ApiResult.error;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.m2rs.core.commons.model.api.response.ApiResult;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class RestAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+        AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        mapper.writeValue(response.getWriter(), error(accessDeniedException));
+
+    }
+}
---
+++ m2rs-user-service/src/main/java/com/m2rs/userservice/security/handler/RestAuthenticationSuccessHandler.java
@@ -0,0 +1,51 @@
+package com.m2rs.userservice.security.handler;
+
+import static com.m2rs.core.commons.model.api.response.ApiResult.ok;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.m2rs.core.security.model.JwtClaim;
+import com.m2rs.core.security.model.JwtClaimInfo;
+import com.m2rs.core.security.utils.JwtUtils;
+import com.m2rs.userservice.model.entity.User;
+import java.io.IOException;
+import java.util.Collections;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class RestAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final JwtClaimInfo jwtClaimInfo;
+
+    public RestAuthenticationSuccessHandler(JwtClaimInfo jwtClaimInfo) {
+        this.jwtClaimInfo = jwtClaimInfo;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
+
+        User user = (User) authentication.getPrincipal();
+
+        response.setStatus(HttpStatus.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        JwtClaim jwtClaim = JwtClaim.builder()
+            .id(user.getId())
+            .departmentId(user.getDepartment().getId())
+            .email(user.getEmail())
+            .name(user.getName())
+            .build();
+
+        String token = JwtUtils.newToken(jwtClaim.toClaims(), jwtClaimInfo);
+
+        mapper.writeValue(response.getWriter(), ok(Collections.singletonMap("token", token)));
+
+    }
+}
