c1f3604cd2bcd15ee3ad08dc3411888029255625
Bob Park <hwpark11@kakao.com>
2022-03-15 17:03:29 +0900

    M2RS-26 feat: jpa audit 기능 활성화

---
+++ m2rs-meeting-room-service/src/main/java/com/m2rs/meetingroomservice/model/entity/base/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.m2rs.meetingroomservice.model.entity.base;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createdDate;
+
+    @LastModifiedDate
+    protected LocalDateTime lastModifiedDate;
+
+}
