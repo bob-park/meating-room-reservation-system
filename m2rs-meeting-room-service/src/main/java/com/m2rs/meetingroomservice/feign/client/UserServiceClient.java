32a9865f9d4e4c3207354f7eb8f912672bf4044c
Bob Park <hwpark11@kakao.com>
2022-03-16 10:23:22 +0900

    M2RS-26 refactor: getCompany API Response 수정

--- m2rs-meeting-room-service/src/main/java/com/m2rs/meetingroomservice/feign/client/UserServiceClient.java
+++ m2rs-meeting-room-service/src/main/java/com/m2rs/meetingroomservice/feign/client/UserServiceClient.java
@@ -1,5 +1,6 @@
 package com.m2rs.meetingroomservice.feign.client;
 
+import com.m2rs.core.commons.model.api.response.ApiResult;
 import com.m2rs.meetingroomservice.model.api.company.CompanyResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface UserServiceClient {
 
     @GetMapping(path = "company/{companyId}")
-    CompanyResponse getCompany(@PathVariable Long companyId);
+    ApiResult<CompanyResponse> getCompany(@PathVariable Long companyId);
 
 
 }
