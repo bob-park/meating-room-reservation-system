154dd5bd5dc8335d51197340532d9c55ffd3c866
Bob Park <hwpark11@kakao.com>
2022-03-16 17:26:35 +0900

    M2RS-26 feat: 회의실 예약 조회 조건 추가

---
+++ m2rs-meeting-room-service/src/main/java/com/m2rs/meetingroomservice/repository/meetingroom/reservation/query/MeetingRoomReservationSearchCondition.java
@@ -0,0 +1,25 @@
+package com.m2rs.meetingroomservice.repository.meetingroom.reservation.query;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MeetingRoomReservationSearchCondition {
+
+    private final LocalDate startDateFrom;
+    private final LocalDate startDateTo;
+
+    private final Long mrId;
+    private final Long userId;
+
+
+    @Builder
+    private MeetingRoomReservationSearchCondition(LocalDate startDateFrom,
+        LocalDate startDateTo, Long mrId, Long userId) {
+        this.startDateFrom = startDateFrom;
+        this.startDateTo = startDateTo;
+        this.mrId = mrId;
+        this.userId = userId;
+    }
+}
