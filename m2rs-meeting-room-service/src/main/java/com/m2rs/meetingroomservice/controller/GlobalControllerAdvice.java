c9d79bf4194e58f0a0a92b9f92762259f1824e64
Bob Park <hwpark11@kakao.com>
2022-03-16 14:06:20 +0900

    M2RS-26 refactor: meeting room service controller advice 추가

---
+++ m2rs-meeting-room-service/src/main/java/com/m2rs/meetingroomservice/controller/GlobalControllerAdvice.java
@@ -0,0 +1,42 @@
+package com.m2rs.meetingroomservice.controller;
+
+import static com.m2rs.core.commons.model.api.response.ApiResult.error;
+
+import com.m2rs.core.commons.exception.NotFoundException;
+import com.m2rs.core.commons.model.api.response.ApiResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public <T> ApiResult<T> badRequest(IllegalArgumentException e) {
+        log.warn(e.getMessage());
+
+        return error(e);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public <T> ApiResult<T> notFound(NotFoundException e) {
+        log.warn(e.getMessage());
+
+        return error(e);
+
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public <T> ApiResult<T> internalServerError(Exception e) {
+        log.error("Service Error - {}", e.getMessage(), e);
+
+        return error(e);
+    }
+
+}
