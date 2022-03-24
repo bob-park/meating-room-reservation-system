0af52702603822599e73fdd0dcc49aa14ada1b20
Bob Park <hwpark11@kakao.com>
2022-03-24 13:56:04 +0900

    M2RS-62 test: company 조회 API 테스트 수정

--- m2rs-user-service/src/test/java/com/m2rs/userservice/controller/controller/company/CompanyControllerTest.java
+++ m2rs-user-service/src/test/java/com/m2rs/userservice/controller/controller/company/CompanyControllerTest.java
@@ -3,9 +3,12 @@ package com.m2rs.userservice.controller.controller.company;
 import java.time.LocalDateTime;
 import java.util.Collections;
 
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -14,7 +17,9 @@ import com.m2rs.userservice.commons.fields.company.CompanyResponseField;
 import com.m2rs.userservice.controller.CommonController;
 import com.m2rs.userservice.model.entity.Company;
 import com.m2rs.userservice.repository.company.CompanyRepository;
+import org.springframework.restdocs.request.ParameterDescriptor;
 
+import static com.m2rs.core.document.utils.SnippetUtils.customPathParamFields;
 import static com.m2rs.core.document.utils.SnippetUtils.customResponseFields;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -42,15 +47,18 @@ class CompanyControllerTest extends CommonController {
         Page<Company> mockCompanies = new PageImpl<>(Collections.singletonList(mockCompany));
 
         when(companyRepository.search(any(), any())).thenReturn(mockCompanies);
+        when(companyRepository.findById(any())).thenReturn(Optional.of(mockCompany));
     }
 
-    @Test
-    @DisplayName("search-company")
-    void searchCompanyTest() throws Exception {
-        mockMvc.perform(get(COMPANY_API))
+    @ParameterizedTest
+    @ValueSource(longs = 1L)
+    @DisplayName("get-company")
+    void getCompanyTest(long comId) throws Exception {
+        mockMvc.perform(get(COMPANY_API + "/{comId}", comId))
             .andDo(print())
             .andExpect(status().isOk())
-            .andDo(document.document(customResponseFields(CompanyResponseField.COMPANY_RESPONSE)));
+            .andDo(document.document(customPathParamFields(),
+                customResponseFields(CompanyResponseField.COMPANY_RESPONSE)));
     }
 
 }
