340c458e4708656e2668eb8fd2d6f745f075f00d
Bob Park <hwpark11@kakao.com>
2022-03-29 17:05:16 +0900

    M2RS-66 test: reservation controller test 전 mock repository 생성

--- m2rs-meeting-room-service/src/test/java/com/m2rs/meetingroomservice/controller/meetingroom/reservation/MeetingRoomReservationControllerTest.java
+++ m2rs-meeting-room-service/src/test/java/com/m2rs/meetingroomservice/controller/meetingroom/reservation/MeetingRoomReservationControllerTest.java
@@ -1,10 +1,16 @@
 package com.m2rs.meetingroomservice.controller.meetingroom.reservation;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import com.m2rs.meetingroomservice.controller.CommonControllerTest;
 import com.m2rs.meetingroomservice.model.entity.MeetingRoom;
 import com.m2rs.meetingroomservice.model.entity.MeetingRoomReservation;
 import com.m2rs.meetingroomservice.repository.meetingroom.MeetingRoomRepository;
 import com.m2rs.meetingroomservice.repository.meetingroom.reservation.MeetingRoomReservationRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -31,16 +37,22 @@ class MeetingRoomReservationControllerTest extends CommonControllerTest {
         MeetingRoomReservation mockReservation =
             MeetingRoomReservation.builder()
                 .id(1L)
+                .title("test title")
+                .startDate(LocalDate.now().atStartOfDay())
+                .endDate(LocalDate.now().atTime(LocalTime.MAX))
                 .build();
+
+        mockReservation.setMeetingRoom(mockMeetingRoom);
 
         /*
          mock meeting room repository
          */
+        when(meetingRoomRepository.findById(any())).thenReturn(Optional.of(mockMeetingRoom));
 
         /*
          mock meeting room reservation repository
          */
-
+        when(meetingRoomReservationRepository.save(any())).thenReturn(mockReservation);
     }
 
 }
